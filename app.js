5ccbcffa37053b4e9582769764ba8200fec4c86a
Matt Tennison <matt@matttennison.com>
2016-04-30 14:38:58 +0100

    Wrote functions to check if a bar is open, and a way to get an array of openBars

--- app.js
+++ app.js
@@ -47,5 +47,39 @@ function getRandomBar(){
   return bars[randomIndex];
 }
 
+//this function gets just the open bars and returns the array
+function getOpenBars(){
+  var date = new Date();
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  var currentTime = hour + "" + minutes;
+  var openBars;
+  
+  //itterate through the bars array
+  for(var i=0; i < bars.length; i++){
+    //if we're past the openTime and before the closeTime
+    if(isOpen(bars[i].startTime, bars[i].endTime, currentTime)){
+       openBars.push(bars[i]);
+    }
+  }
+  
+  return openBars;
+}
+
+function isOpen(startTime, endTime, currentTime){
+  //to handle the midnight to 4am slot (best time for drinking trebles responsibly...)
+  if(currentTime > 0 && currentTime < 4){
+    currentTime = currentTime + 24;
+    endTime = endTime + 24;
+  }
+  
+  if(currentTime > startTime && currentTime < endTime){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
 http.createServer(bot.middleware()).listen(3000);
 console.log('Echo bot server running at port 3000.');
